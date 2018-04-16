ae03daf41c48a84527d4a88bc8d188475abd986b
Cameron Ross <elpenguino@gmail.com>
2018-04-16 03:35:08 -0300

    add basic disgaea 4 support

--- source/disgaeareporter/disgaea4/common.d
+++ source/disgaeareporter/disgaea4/common.d
@@ -1,1 +1,42 @@
 module disgaeareporter.disgaea4.common;
+
+import d4data;
+
+
+string skillName(ushort id) {
+	import std.conv : to;
+	if (id in d4skillNames) {
+		return d4skillNames[id];
+	}
+	return "Unknown skill "~id.to!string(16);
+}
+
+string className(ushort id) {
+	import std.conv : to;
+	if (id in d4classes) {
+		return d4classes[id];
+	}
+	return "Unknown class "~id.to!string(16);
+}
+
+string itemName(ushort id) {
+	import std.conv : to;
+	if (id in d4items) {
+		return d4items[id];
+	}
+	return "Unknown item "~id.to!string(16);
+}
+string mapName(ushort id) {
+	import std.conv : to;
+	if (id in d4mapNames) {
+		return d4mapNames[id];
+	}
+	return "Unknown map "~id.to!string(16);
+}
+string innocentName(const ushort id) {
+	import std.conv : to;
+	if (id in d4innocents) {
+		return d4innocents[id];
+	}
+	return "Unknown innocent "~id.to!string(16);
+}
--- source/disgaeareporter/disgaea4/ps3.d
+++ source/disgaeareporter/disgaea4/ps3.d
@@ -1,6 +1,104 @@
 module disgaeareporter.disgaea4.ps3;
 
+import disgaeareporter.common;
+import disgaeareporter.disgaea4.common;
+
+import std.range : isOutputRange;
+
+align(1)
+struct Innocent {
+	align(1):
+	BigEndian!uint level;
+	BigEndian!ushort type;
+	@Unknown ubyte[2] unknown;
+	void toString(T)(T sink) const if (isOutputRange!(T, const(char))) {
+		import std.format;
+		sink.formattedWrite!"Lv%s%s %s"(level, /+level > 10000 ? "+" : ""+/ "", type.innocentName);
+	}
+	bool isValid() const {
+		return type != 0;
+	}
+}
+
+align(1)
+struct Item {
+	align(1):
+	@Unknown ubyte[4] unknown1;
+	Innocent[8] innocents;
+	@Unknown ubyte[140] unknown2;
+	BigEndian!ushort id;
+	@Unknown ubyte[30] unknown3;
+	ubyte rarity;
+	@Unknown ubyte[30] unknown4;
+	ZeroString!64 name;
+	@Unknown ubyte[0x61] unknown5;
+	bool isValid() const {
+		return id != 0;
+	}
+	void toString(T)(T sink) const if (isOutputRange!(T, const(char))) {
+		import std.algorithm : filter;
+		import std.format;
+		sink.formattedWrite!"Lv%s %s (Rarity: %s) - %(%s, %)"("?"/+level+/, name, rarity, innocents[].filter!(x => x.isValid));
+		debug (unknowns) {
+			sink.formattedWrite!" - Unknown data:"();
+			import std.traits : getSymbolsByUDA;
+			static foreach (i; 0..getSymbolsByUDA!(typeof(this), Unknown).length) {
+				sink.formattedWrite!"(%s)"(getSymbolsByUDA!(typeof(this), Unknown)[i]);
+			}
+		}
+	}
+}
+static assert(Item.sizeof == 0x1B0);
+static assert(Item.rarity.offsetof == 0xF0);
+
+align(1)
+struct Character {
+	align(1):
+	BigEndian!ulong exp;
+	Item[4] equipment;
+	ZeroString!48 name;
+	ZeroString!48 className;
+	@Unknown ubyte[1128] unknown1;
+	BigEndian!ulong currentHP;
+	BigEndian!ulong currentSP;
+	LongStats!true stats;
+	LongStats!true realStats;
+	@Unknown ubyte[912] unknown2;
+	BigEndian!ushort level;
+	@Unknown ubyte[12854] unknown3;
+}
+
+static assert(Character.sizeof == 0x41E8);
+static assert(Character.currentHP.offsetof == 0xB90);
+static assert(Character.level.offsetof == 0xFB0);
+
 align(1)
 struct D4PS3 {
 	align(1):
+	@Unknown ubyte[8] unknown1;
+	Playtime!true playtime;
+	ZeroString!34 fileName;
+	@Unknown ubyte[5041] unknown2;
+	Character[64] _characters;
+	@Unknown ubyte[336176] unknown3;
+	Item[32] _bagItems;
+	Item[512] _warehouseItems;
+	@Unknown ubyte[34828] unknown4;
+	BigEndian!ushort charCount;
+
+	auto characters() const {
+		return _characters[0..charCount];
+	}
+	auto bagItems() const {
+		import std.algorithm : filter;
+		return _bagItems[].filter!(x => x.isValid);
+	}
+	auto warehouseItems() const {
+		import std.algorithm : filter;
+		return _warehouseItems[].filter!(x => x.isValid);
+	}
 }
+
+static assert(D4PS3._characters.offsetof == 0x13E0);
+static assert(D4PS3._bagItems.offsetof == 0x15AF10);
+static assert(D4PS3.charCount.offsetof == 0x19CD1C);
